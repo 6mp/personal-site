9d914defce17a8da39a1e092b4a6975dfe228b6d
matt <30198937+6mp@users.noreply.github.com>
2022-05-02 12:55:51 -0400

    login button

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import {Grid, Input, Page, Spacer, Text} from '@geist-ui/core'
+import {Button, Grid, Input, Note, Page, Spacer, Text} from '@geist-ui/core'
 
 const Home = () => {
     return (
@@ -11,18 +11,22 @@ const Home = () => {
             <Page dotBackdrop width="800px" padding={0}>
                 <Grid.Container justify={"center"}>
                     <Grid xs={24} justify={"center"}>
-                        <Text h1>Todo List Senior Project</Text>
+                        <Text style={{textAlign: "center"}} h1>Todo List Senior Project</Text>
                     </Grid>
                     <Grid xs={24} justify={"center"}>
-                        <Input label="Username"/>
-                    </Grid>
-                    <Spacer/>
-                    <Grid xs={24} justify={"center"}>
-                        <Input.Password label="Password"/>
+                        <Input width={17} label="Username"/>
                     </Grid>
                     <Spacer/>
                     <Grid xs={24} justify={"center"}>
-                        {/*<Text h3>If you do not have an account one will be made upon login</Text>*/}
+                        <Input.Password width={17} label="Password"/>
+                    </Grid>
+                    <Spacer/>
+                    <Grid xs={24} justify={"center"}>
+                        <Button>Login</Button>
+                    </Grid>
+                    <Spacer/>
+                    <Grid xs={24} justify={"center"}>
+                        <Note>If you do not have an account one will be made upon login</Note>
                     </Grid>
                 </Grid.Container>
             </Page>
