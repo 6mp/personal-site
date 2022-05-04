c7efc1c832488d83170cf81b87219788f428f6b5
matt <30198937+6mp@users.noreply.github.com>
2022-05-04 14:14:23 -0400

    push out

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,37 +1,87 @@
 import Head from 'next/head'
-import {Button, Grid, Input, Note, Page, Spacer, Text} from '@geist-ui/core'
+import {Button, Grid, Input, Note, Page, Spacer, Text, useInput, useToasts} from '@geist-ui/core'
+import {useRouter} from "next/router";
+import {useEffect} from "react";
+import AES from 'crypto-js/aes';
 
 const Home = () => {
+
+    const {state: usernameValue, bindings: usernameBindings} = useInput("");
+    const {state: passwordValue, bindings: passwordBindings} = useInput("");
+    const {setToast} = useToasts()
+    const router = useRouter()
+
+    useEffect(() => {
+
+    })
+
+    const login = async (): Promise<{ success: boolean, content: string }> => {
+        const myHeaders = new Headers();
+        myHeaders.append("username", usernameValue);
+        myHeaders.append("password", passwordValue);
+
+        const requestOptions = {
+            method: 'GET',
+            headers: myHeaders,
+        };
+
+        const response = await fetch("http://127.0.0.1:8787/login", requestOptions);
+        const json = await response.json();
+        return {success: json.success, content: json.content}
+    }
+
     return (
         <div>
             <Head>
-                <title>Todo List</title>
-                <link rel="icon" href="/favicon.ico"/>
+                <title>To Do List</title>
+                <link rel={"icon"} href={"/favicon.ico"}/>
             </Head>
-            <Page dotBackdrop width="800px" padding={0}>
+            <Page dotBackdrop width={"800px"} padding={0}>
                 <Grid.Container justify={"center"}>
                     <Grid xs={24} justify={"center"}>
-                        <Text style={{textAlign: "center"}} h1>Todo List Senior Project</Text>
+                        <Text style={{textAlign: "center"}} h1>To Do List Senior Project</Text>
                     </Grid>
                     <Grid xs={24} justify={"center"}>
-                        <Input width={17} label="Username"/>
-                    </Grid>
-                    <Spacer/>
-                    <Grid xs={24} justify={"center"}>
-                        <Input.Password width={17} label="Password"/>
+                        <Input width={17} label={"Username"} {...usernameBindings}/>
                     </Grid>
                     <Spacer/>
                     <Grid xs={24} justify={"center"}>
-                        <Button>Login</Button>
+                        <Input.Password width={17} label={"Password"} {...passwordBindings}/>
                     </Grid>
                     <Spacer/>
                     <Grid xs={24} justify={"center"}>
-                        <Note>If you do not have an account one will be made upon login</Note>
+                        <Button width={1.6} onClick={async () => {
+
+                            const ciphertext = AES.encrypt("sss", 'secretPassphrase');
+                            console.log(ciphertext.toString())
+
+                            const {success, content} = await login();
+
+                            if (!success) {
+                                setToast({
+                                    text: content,
+                                    type: "error",
+                                });
+                            } else {
+                                setToast({
+                                    text: content,
+                                });
+
+                                sessionStorage.setItem("username", usernameValue);
+                                sessionStorage.setItem("password", passwordValue);
+
+                                await router.push("list");
+                            }
+                        }}>Login</Button>
+                    </Grid>
+                    <Spacer/>
+                    <Grid xs={24} justify={"center"}>
+                        <Note style={{textAlign: "center"}}><br/>If you do not have an account one will be made upon login</Note>
                     </Grid>
                 </Grid.Container>
             </Page>
         </div>
-    )
+    );
 }
 
 export default Home
---
+++ pages/list.tsx
@@ -0,0 +1,12 @@
+import {Text} from "@geist-ui/core";
+
+const List = () => {
+
+
+
+    return (
+        <Text>Yes</Text>
+    );
+}
+
+export default List
