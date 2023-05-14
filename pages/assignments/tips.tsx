55849aeecd71e3cff3b2d2e20aea1d9d35bfe13c
matt <30198937+6mp@users.noreply.github.com>
2023-05-14 12:25:17 -0400

    fixed

--- pages/assignments/tips.tsx
+++ pages/assignments/tips.tsx
@@ -2,8 +2,10 @@ import {Display, Image} from "@geist-ui/core";
 
 export default function Tips() {
     return (
-        <video width="25%" height="25%"
-               src={"https://stream.mux.com/tlH02rAC00v8w025Nk9EUIQ1ClqxcjmO02dYrPqOcK1xKPw.m3u8?redundant_streams=true"}
-               controls></video>
+        <div className="iframe-container">
+            <iframe
+                src="https://lite-youtube-embed-iframe.vercel.app/embed/EjbTPsgjQ10"
+                loading="lazy"></iframe>
+        </div>
     );
 }
